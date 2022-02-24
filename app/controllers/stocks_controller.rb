8be157b7c58671c0a9b7d3552ded776e4fa002ff
Carlos Ramos <carlosr9m@gmail.com>
2022-02-24 01:20:07 +0100

    search stocks using AJAX

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,26 @@
 class StocksController < ApplicationController
 
   def search
+    if params[:stock].present?
+      search_stock(params[:stock])
+    else
+      flash[:alert] = 'Please insert a symbol to search'
+      redirect_to my_portfolio_path
+    end
+  end
+
+  private
+
+  def search_stock(symbol)
     @stock = Stock.new_lookup(params[:stock])
-    render 'users/my_portfolio'
+    if @stock
+      respond_to do |format|
+        format.js { render partial: 'users/result' }
+      end
+    else
+      flash[:alert] = 'Invalid symbol! Try another one?'
+      redirect_to my_portfolio_path
+    end
   end
 
 end
